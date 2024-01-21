6be6223460e8f3f7ee0c18abf5140dbe1107c78e
Gyaneshwar Tiwari <gyantiwari2303@gmail.com>
2024-01-21 20:00:03 +0530

    style modifications of learn more

--- client/src/components/Learnmore/Learnmore.jsx
+++ client/src/components/Learnmore/Learnmore.jsx
@@ -6,6 +6,7 @@ import works from "../images/works.jpg";
 import empower from "../images/empower.jpg";
 import collaborate from "../images/collaborate.jpg";
 import { Link } from "react-router-dom";
+import './Learnmore.css'
 
 const Learnmore = () => {
   return (
@@ -26,18 +27,18 @@ const Learnmore = () => {
             Bridging the Gap Between Excess and Necessity
           </h2>
           <p class="title-text" style={{ color: "white" }}>
-          In response to the coexistence of food wastage and hunger, 
-          the Zero Hunger Initiative pioneers innovative solutions by 
-          bridging surplus restaurant food with critical needs in impoverished areas. 
-          This groundbreaking project not only addresses food wastage but also makes a 
-          meaningful impact on communities struggling with hunger. AnnaDaata acts as a crucial link, 
-          seamlessly connecting individuals, organizations, restaurants, and marketplace 
-          contributors to those in need, fostering a community-driven effort to combat hunger
-           and reduce food waste. Join us in building a network where every contributor plays a vital role
+            In response to the coexistence of food wastage and hunger,
+            the Zero Hunger Initiative pioneers innovative solutions by
+            bridging surplus restaurant food with critical needs in impoverished areas.
+            This groundbreaking project not only addresses food wastage but also makes a
+            meaningful impact on communities struggling with hunger. AnnaDaata acts as a crucial link,
+            seamlessly connecting individuals, organizations, restaurants, and marketplace
+            contributors to those in need, fostering a community-driven effort to combat hunger
+            and reduce food waste. Join us in building a network where every contributor plays a vital role
             in ensuring a hunger-free future for all.
           </p>
         </div>
-        <div class="imgBx">
+        <div class="w-[50%]">
           <img
             src="https://media.licdn.com/dms/image/D4E12AQGZN3PxH4quRA/article-cover_image-shrink_720_1280/0/1698593727046?e=2147483647&v=beta&t=Gl3kRN2TKRCGhNlUqmz4Xc6VmZoEyyzTyQmElQjMUlQ"
             alt=""
@@ -48,7 +49,7 @@ const Learnmore = () => {
       <div class="post container">
         <div class="post-box tech">
           <img src={problem} alt="" class="problem-img" />
-          <Link to="/" class="post-title">
+          <Link to="/" class="post-title my-1">
             The Problem:
           </Link>
           <p class="post-description">
@@ -61,44 +62,44 @@ const Learnmore = () => {
           </p>
         </div>
         <div class="post-box food">
-          <img src={works} alt="" class="post-img" />
+          <img src={works} alt="" class="post-img my-2" />
           <Link to="/" class="post-title">
             How It Works:
           </Link>
           <p class="post-description">
-            The concept is simple yet powerful. AnnaDaata connects contributors 
-            through our platform, scheduling pickups for surplus food. 
-            Efficient logistics distribute to strategically placed kiosks, 
-            providing secure access for those in need. Join our community-driven 
-            network to combat hunger collaboratively. Every contributor, 
+            The concept is simple yet powerful. AnnaDaata connects contributors
+            through our platform, scheduling pickups for surplus food.
+            Efficient logistics distribute to strategically placed kiosks,
+            providing secure access for those in need. Join our community-driven
+            network to combat hunger collaboratively. Every contributor,
             big or small, plays a crucial role in ensuring no one goes hungry.
           </p>
         </div>
         <div class="post-box tech">
-          <img src={img7} alt="" class="post-img" />
+          <img src={img7} alt="" class="post-img my-2" />
           <Link to="/" class="post-title">
             Technology and Logistics:
           </Link>
           <p class="post-description">
-          AnnaDaata employs innovative solutions to seamlessly connect 
-          food contributors with those in need. Leveraging advanced logistics, 
-          our platform ensures efficient scheduling, collection, and distribution of 
-          surplus food to strategically positioned kiosks. This harmonious integration 
-          of technology and logistics forms the backbone of our initiative, 
-          facilitating a swift and impactful response to the urgent challenges of hunger and food waste.
+            AnnaDaata employs innovative solutions to seamlessly connect
+            food contributors with those in need. Leveraging advanced logistics,
+            our platform ensures efficient scheduling, collection, and distribution of
+            surplus food to strategically positioned kiosks. This harmonious integration
+            of technology and logistics forms the backbone of our initiative,
+            facilitating a swift and impactful response to the urgent challenges of hunger and food waste.
           </p>
         </div>
         <div class="post-box news">
-          <img src= "https://th.bing.com/th/id/OIP.0JuXJ-WkdBPdAMZnFANohAAAAA?rs=1&pid=ImgDetMain" alt="Empowering Communities" class="post-img" />
+          <img src="https://th.bing.com/th/id/OIP.0JuXJ-WkdBPdAMZnFANohAAAAA?rs=1&pid=ImgDetMain" alt="Empowering Communities" class="post-img" />
           <Link to="/" class="post-title">
             Empowering Communities:
           </Link>
           <p class="post-description">
-          AnnaDaata empowers communities through collaboration, 
-          connecting contributors and those in need to combat hunger. 
-          Our platform creates a network where everyone contributes to 
-          building a stronger, more resilient community, addressing the 
-          critical issue of hunger together.
+            AnnaDaata empowers communities through collaboration,
+            connecting contributors and those in need to combat hunger.
+            Our platform creates a network where everyone contributes to
+            building a stronger, more resilient community, addressing the
+            critical issue of hunger together.
           </p>
         </div>
       </div>
--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Loading.css'; // Make sure to import the CSS file
+import './Loading.css';
 
 const Loading = () => {
     return (
