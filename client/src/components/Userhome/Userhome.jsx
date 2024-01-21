a8f7e78739ec1f4c260be14396d3ad76f1803593
Abhishek Pandey <arshh0509@gmail.com>
2024-01-21 17:48:56 +0530

    updated view

--- client/src/components/Userhome/Userhome.jsx
+++ client/src/components/Userhome/Userhome.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Sidebar from '../Sidebar/Sidebar'
-import Leaderboard from '../Leaderboard/Leaderboard.jsx'
+import React from 'react';
 import { useSelector } from 'react-redux';
-import Pickup from "../Pickup/Recentpickups.jsx";
+import Leaderboard from '../Leaderboard/Leaderboard.jsx';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Userhome = () => {
     const { user } = useSelector(state => state.auth);
     return (
         <div className='flex justify-between mx-8'>
             <div><Sidebar /></div>
-            <div className='my-4 bg-gray-100 p-4 rounded-lg shadow-md h-[80vh]  overflow-auto'>
+            <div className='my-4 bg-gray-100 p-4 rounded-lg shadow-md overflow-auto'>
                 <h2 className='text-2xl font-bold mb-4 text-gray-800'>Recent Contributions</h2>
                 <div className='w-[35vw]'>
                     {user && user.pickups.map((item) => (
@@ -24,7 +23,7 @@ const Userhome = () => {
                 </div>
             </div>
 
-            <div className='h-[82vh] overflow-auto'><Leaderboard /></div>
+            <div><Leaderboard /></div>
         </div>
     )
 }
